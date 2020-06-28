a180043a2025502b806cd2acfe026cf0964bce39
Vyshnav-KS <62277580+Vyshnav-KS@users.noreply.github.com>
2020-06-28 23:27:46 +1245

    Loader added

--- myscript.js
+++ myscript.js
@@ -1,3 +1,13 @@
 function myFunction(x) {
     x.classList.toggle("change");
   }
+  var myVar;
+
+function myFunctionL() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
